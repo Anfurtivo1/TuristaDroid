5bcd8028f7e73afc7fe714e009d5600b30daba7d
Andres <anfurtivo1@hotimail.com>
2020-12-04 00:23:24 +0100

    BotonEncenderLinterna
    Se ha conseguido que se encienda la linterna

--- app/src/main/java/com/andresivan/turistadroid/pantallaprincipal.kt
+++ app/src/main/java/com/andresivan/turistadroid/pantallaprincipal.kt
@@ -1,22 +1,36 @@
 package com.andresivan.turistadroid
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.hardware.Camera
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+
 
 class pantallaprincipal : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    lateinit var camera: Camera
+    var parameters: Camera.Parameters? = null
+    var isFlash = false
+    var estado = true
+    var permisos:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +56,8 @@ class pantallaprincipal : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -50,8 +66,62 @@ class pantallaprincipal : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.EncenderLinterna -> {
+
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.CAMERA
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1000)
+                } else {
+                    if (getApplicationContext().getPackageManager()
+                            .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
+                    ) {
+                        camera = Camera.open();
+                        parameters = camera.getParameters();
+                        isFlash = true;
+                    }
+
+                }
+
+                encenderLinterna()
+
+                val duracion = Toast.LENGTH_SHORT
+                val toast = Toast.makeText(applicationContext, R.string.EncenderLinterna, Toast.LENGTH_SHORT)
+                toast.show()
+                true
+
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    fun encenderLinterna() {
+
+        val camManager =
+            getSystemService(CAMERA_SERVICE) as CameraManager
+        val cameraId =
+            camManager.cameraIdList[0]
+        if (estado){
+            camManager.setTorchMode(cameraId, true)
+            estado=false
+        }else{
+            camManager.setTorchMode(cameraId, false)
+            estado=true
+        }
+
+
+    }
+
 }
